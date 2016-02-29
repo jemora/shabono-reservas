3b3b0b594a4a81e269c4dd717e38f5356ecfce5e
Jaime Ernesto Mora <jemora70@gmail.com>
2016-02-28 22:36:27 -0430

    Carga inicial conector, connections, imagenes

---
+++ Connections/connFlashblog.php
@@ -0,0 +1,7 @@
+<?php
+$hostname_connFlashblog = "localhost";
+$database_connFlashblog = "hotel";
+$username_connFlashblog = "root";
+$password_connFlashblog = "";
+$connFlashblog= mysql_connect($hostname_connFlashblog, $username_connFlashblog, $password_connFlashblog) or die(mysql_error());
+?>
---
+++ conector/conector.php
@@ -0,0 +1,8 @@
+<?php
+$hostname = "localhost";
+$database = "hotel";
+$username = "root";
+$password = "";
+$connFlashblog = mysql_connect($hostname, $username, $password) or die(mysql_error());
+?>
+
